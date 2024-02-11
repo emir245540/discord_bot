1c8f7472f878db6fdfed81437964158d11ae2376
emir245540 <157307747+emir245540@users.noreply.github.com>
2024-02-11 21:21:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def carpma(ctx, sayi1, sayi2 ):
+    await ctx.send(f"Sayıların Çarpımı: {int(sayi1) * int(sayi2)}")
+
+bot.run("MTIwMzc1Mjc3NzM3MzcxNjUxMA.G6M7oP.K-lIaFJiPlNIqk5ayvqm_nX9lGahfy_OXhBM5I")
